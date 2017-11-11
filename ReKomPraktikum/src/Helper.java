67c26a975289de8a29769bee522de2111cc5c183
Nico Vogel <nico-vogel97@outlook.de>
2017-11-12 00:53:33 +0100

    implemented startup check for the receiver.

--- ReKomPraktikum/src/Helper.java
+++ ReKomPraktikum/src/Helper.java
@@ -2,13 +2,45 @@
 public final class Helper {
 
 	private static long baseTime = System.currentTimeMillis();
-	
+
 	private Helper() {
-		
+
 	}
-	
+
 	public static String GetMilliTime() {
 		return String.valueOf(System.currentTimeMillis() - baseTime);
 	}
-	
+
+	/**
+	 * Check if the given string can be converted into a short value
+	 * 
+	 * @param strShort
+	 *            check if this string is a valid short
+	 * @return
+	 */
+	public static boolean tryParceShort(String strShort) {
+		try {
+			Short.parseShort(strShort);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Check if the given string can be converted into a integer value
+	 * 
+	 * @param strInt
+	 *            check if this string is a valid integer
+	 * @return
+	 */
+	public static boolean tryParceInt(String strInt) {
+		try {
+			Integer.parseInt(strInt);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		return true;
+	}
+
 }
--- ReKomPraktikum/src/RNReceiver.java
+++ ReKomPraktikum/src/RNReceiver.java
@@ -1,23 +1,44 @@
-import java.io.FileOutputStream;
 import java.io.IOException;
-
-import rn.NetworkCard;
 
 public class RNReceiver {
 
 	public static void main(String[] args) {
-		
-		
-		short sourceAdress = Short.parseShort(args[0]);
-		short destinationAdress = Short.parseShort(args[1]);
-		int windowSize = Integer.parseInt(args[2]);
-		
+
 		try {
-			FrameReceiver receiver = new FrameReceiver(sourceAdress, destinationAdress, windowSize);
+
+			if (args == null)
+				throw new IllegalArgumentException("The given startup arguments are null.");
+
+			if (args.length < 3)
+				throw new IllegalArgumentException(
+						"There must be at least 3 startup arguments for this receiver to run.");
+
+			if (Helper.tryParceShort(args[0]) == false)
+				throw new IllegalArgumentException(
+						"The first startup argument is not a number or the size is too big (between -32.768 and 32.767). Its used as a a source address");
+
+			if (Helper.tryParceShort(args[1]) == false)
+				throw new IllegalArgumentException(
+						"The second startup argument is not a number or the size is too big (between -32.768 and 32.767). Its used as a destination address");
+
+			if (Helper.tryParceInt(args[2]) == false)
+				throw new IllegalArgumentException(
+						"The third startup argument is not a number or the size is too big (between –2.147.483.648 and 2.147.483.647). Its used for the window size.");
+
+			short sourceAddress = Short.parseShort(args[0]);
+			short destinationAddress = Short.parseShort(args[1]);
+			int windowSize = Integer.parseInt(args[2]);
+
+			FrameReceiver receiver = new FrameReceiver(sourceAddress, destinationAddress, windowSize);
 		} catch (IOException e) {
 			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			System.out.println(String.format("There was a problem with the startup arguments: %s\n%s", e.getMessage(),
+					e.getStackTrace()));
+		} catch (Exception e) {
+			System.out.println(String.format("An unexpected error occurred with the following message: %s\n%s",
+					e.getMessage(), e.getStackTrace()));
 		}
 	}
-
 
 }
