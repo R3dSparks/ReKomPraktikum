74e21699d948e9e934bcafe56c9d784c9e436480
Nico Vogel <nico-vogel97@outlook.de>
2017-11-12 01:08:23 +0100

    refactored RNReceiver class. moved the checks into a seperate method

--- ReKomPraktikum/src/RNReceiver.java
+++ ReKomPraktikum/src/RNReceiver.java
@@ -6,24 +6,7 @@ public class RNReceiver {
 
 		try {
 
-			if (args == null)
-				throw new IllegalArgumentException("The given startup arguments are null.");
-
-			if (args.length < 3)
-				throw new IllegalArgumentException(
-						"There must be at least 3 startup arguments for this receiver to run.");
-
-			if (Helper.tryParceShort(args[0]) == false)
-				throw new IllegalArgumentException(
-						"The first startup argument is not a number or the size is too big (between -32.768 and 32.767). Its used as a a source address");
-
-			if (Helper.tryParceShort(args[1]) == false)
-				throw new IllegalArgumentException(
-						"The second startup argument is not a number or the size is too big (between -32.768 and 32.767). Its used as a destination address");
-
-			if (Helper.tryParceInt(args[2]) == false)
-				throw new IllegalArgumentException(
-						"The third startup argument is not a number or the size is too big (between –2.147.483.648 and 2.147.483.647). Its used for the window size.");
+			checkStartupArguments(args);
 
 			short sourceAddress = Short.parseShort(args[0]);
 			short destinationAddress = Short.parseShort(args[1]);
@@ -39,6 +22,32 @@ public class RNReceiver {
 			System.out.println(String.format("An unexpected error occurred with the following message: %s\n%s",
 					e.getMessage(), e.getStackTrace()));
 		}
+	}
+
+	/**
+	 * check if the startup arguments are valid
+	 * 
+	 * @param args
+	 */
+	private static void checkStartupArguments(String[] args) {
+		if (args == null)
+			throw new IllegalArgumentException("The given startup arguments are null.");
+
+		if (args.length < 3)
+			throw new IllegalArgumentException("There must be at least 3 startup arguments for this receiver to run.");
+
+		if (Helper.tryParceShort(args[0]) == false)
+			throw new IllegalArgumentException(
+					"The first startup argument is not a number or the size is too big (between -32.768 and 32.767). Its used as a a source address");
+
+		if (Helper.tryParceShort(args[1]) == false)
+			throw new IllegalArgumentException(
+					"The second startup argument is not a number or the size is too big (between -32.768 and 32.767). Its used as a destination address");
+
+		if (Helper.tryParceInt(args[2]) == false)
+			throw new IllegalArgumentException(
+					"The third startup argument is not a number or the size is too big (between –2.147.483.648 and 2.147.483.647). Its used for the window size.");
+
 	}
 
 }
