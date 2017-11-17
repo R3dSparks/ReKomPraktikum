3ce8544615657dae536e8faf0489a5a687f8a6df
Nico Vogel <nico-vogel97@outlook.de>
2017-11-17 20:36:30 +0100

    test

--- ReKomPraktikum/src/FrameReceiver.java
+++ ReKomPraktikum/src/FrameReceiver.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -5,7 +6,7 @@ import rn.NetworkCard;
 import rn.Receiver;
 
 public class FrameReceiver implements Receiver {
-	
+
 	private NetworkCard nc;
 	private short adress;
 	private short sourceAdress;
@@ -35,12 +36,12 @@ public class FrameReceiver implements Receiver {
 
 	@Override
 	public void receive(byte[] arg0) {
-		
+
 		// Check parameters
 		if (parametrsAreValid(arg0) == false)
 			return;
 
-		// Read frame data			
+		// Read frame data
 		cleanUpTerminator();
 		Frame dataFrame = new Frame(arg0);
 
@@ -50,11 +51,12 @@ public class FrameReceiver implements Receiver {
 				this.terminator = new TimedTerminator(1000);
 				this.terminator.start();
 			}
-			
-			return;
-		}				
 
-		// Set terminating flag true if received frame is terminating and is the last frame to be acknowledged
+			return;
+		}
+
+		// Set terminating flag true if received frame is terminating and is the
+		// last frame to be acknowledged
 		if (dataFrame.isTerminating() && dataFrame.getSequenceNumber() == this.lastFrameAcknowledged + 1) {
 			this.terminating = true;
 			this.lastFrameAcknowledged++;
@@ -63,19 +65,19 @@ public class FrameReceiver implements Receiver {
 		// Resending acknowledge
 		if (dataFrame.getSequenceNumber() <= this.lastFrameAcknowledged) {
 
-			Frame ackFrame = new Frame(this.adress, this.sourceAdress, this.lastFrameAcknowledged,
-					new byte[0], true, this.terminating);
+			Frame ackFrame = new Frame(this.adress, this.sourceAdress, this.lastFrameAcknowledged, new byte[0], true,
+					this.terminating);
 
-			System.out.println(Helper.GetMilliTime() + ": Resending acknowledge for frame " + ackFrame.getSequenceNumber()
-					+ " to " + ackFrame.getDestinationAddress());
+			System.out.println(Helper.GetMilliTime() + ": Resending acknowledge for frame "
+					+ ackFrame.getSequenceNumber() + " to " + ackFrame.getDestinationAddress());
 
 			this.send(ackFrame);
 
 		} else
 		// Sending acknowledge for new frame within window size
 		if (dataFrame.getSequenceNumber() <= this.lastFrameAcknowledged + this.windowSize) {
-			
-			if(dataFrame.isTerminating() == true) {
+
+			if (dataFrame.isTerminating() == true) {
 				System.out.println(Helper.GetMilliTime() + ": Received terminating frame");
 			} else {
 				System.out.println(Helper.GetMilliTime() + ": Received frame " + dataFrame.getSequenceNumber());
@@ -91,55 +93,59 @@ public class FrameReceiver implements Receiver {
 					writeFrame = this.buffer.GetNextFrame();
 				}
 			}
-			
-			Frame ackFrame = new Frame(this.adress, this.sourceAdress, this.lastFrameAcknowledged,
-					new byte[0], true, this.terminating);
+
+			Frame ackFrame = new Frame(this.adress, this.sourceAdress, this.lastFrameAcknowledged, new byte[0], true,
+					this.terminating);
 
 			this.send(ackFrame);
 
-			if(this.terminating == true) {
-				System.out.println(Helper.GetMilliTime() + ": Sending termination to " + ackFrame.getDestinationAddress());
+			if (this.terminating == true) {
+				System.out.println(
+						Helper.GetMilliTime() + ": Sending termination to " + ackFrame.getDestinationAddress());
 			} else {
 				System.out.println(Helper.GetMilliTime() + ": Sending acknowledge for " + ackFrame.getSequenceNumber()
-				+ " to " + ackFrame.getDestinationAddress());
+						+ " to " + ackFrame.getDestinationAddress());
 			}
-			
+
 		}
 
 		if (this.terminating) {
 			this.terminator = new TimedTerminator(1000);
 			this.terminator.start();
 		}
-		
+
 	}
 
 	private void writeLine(byte[] data) {
-		
-		FileOutputStream fos = null;
-		boolean success = false;
-		
-		while(success == false) {
-			try {
-				fos = new FileOutputStream(this.savePath, true);
-				
-				fos.write(data);
-				
-			} catch (IOException e) {
-				System.out.println("Failed to write to output. Trying again.");
-			} finally {
-				if(fos != null) {
-					try {
-						fos.close();
-					} catch (IOException e) {
-						System.out.println("Closing output stream failed");
-					}	
-				}
 
-				success = true;
-			}		
+		FileOutputStream fos = null;
+		File file = null;
+		try {
+
+			file = new File(this.savePath);
+
+			// make sure that the file exists
+			if (file.exists() == false)
+				file.createNewFile();
+
+			// append the data to the file
+			fos = new FileOutputStream(file, true);
+			fos.write(data);
+			fos.close();
+
+		} catch (IOException e) {
+			System.out.println("Failed to write to output. Trying again.");
+		} finally {
+			if (fos != null) {
+				try {
+					fos.close();
+				} catch (IOException e) {
+					System.out.println("Closing output stream failed");
+				}
+			}
+
 		}
-		
-		
+
 	}
 
 	/**
@@ -178,11 +184,13 @@ public class FrameReceiver implements Receiver {
 	 * @return
 	 */
 	private boolean receivedFrameIsValid(Frame dataFrame) {
-		// Return true if frame is valid, the destination address is correct and the acknowledge flag is false 
-		if (dataFrame.isValid() && dataFrame.getDestinationAddress() == this.adress && dataFrame.isAcknowledge() == false) {
+		// Return true if frame is valid, the destination address is correct and
+		// the acknowledge flag is false
+		if (dataFrame.isValid() && dataFrame.getDestinationAddress() == this.adress
+				&& dataFrame.isAcknowledge() == false) {
 			return true;
 		}
-		
+
 		// If the frame is not valid, try to print its checksum
 		System.out.print(Helper.GetMilliTime() + ": Received invalid frame. ");
 		try {
